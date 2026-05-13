1f2a081f69233a1fd8af1b320fa95a2d15e365fe
adele.douin <adele.douin@gmail.com>
2026-05-13 10:15:38 +0200

    Clean Git Projects : 1) rangement 3) ToDo make a requierement.txt + remplire le README !

--- KnitAnalyse_autorun.sh
+++ KnitAnalyse_autorun.sh
@@ -4,8 +4,6 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate LabQuakes
 
-cd ~/KNITQUAKES/Git/KnitAnalyse_open/Remote_Scripts
-
 #### analyse scalar flu on single data set
 
 for j in 201029_exp1_  201029_exp2_ 201029_exp3_ 201029_exp4_ 201029_exp5_ 201029_exp6_ 201029_exp7_ 201029_exp8_ 201029_exp8_ 201116_exp4_ 201119_exp1_ 201119_exp2_ 201119_exp3_ 201119_exp4_ 201119_exp5_ 201119_exp6_ 201119_exp7_ 201119_exp8_ 201119_exp9_ 201119_exp10_
